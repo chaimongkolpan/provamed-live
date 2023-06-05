b65927afc546a941664cef5f352b4b6d8e5bf960
Chaimongkolpan <cpanawasri@gmail.com>
2023-06-05 16:59:31 +0700

    change random method

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ let ramdomInterval = null;
 let customers = [];
 let master_data = [];
 let lucky1_limit = 1;
-let lucky3_limit = 3;
+let lucky3_limit = 2;
 let lucky10_limit = 10;
+let lucky20_limit = 20;
 let lucky1_count = 0;
 let lucky3_count = 0;
 let lucky10_count = 0;
+let lucky20_count = 0;
 let lucky1 = [];
 let lucky3 = [];
 let lucky10 = [];
+let lucky20 = [];
 $(document).ready(function() {
     function readFile() {
         let file = $('#customer-file')[0].files[0];
@@ -187,28 +190,114 @@ $(document).ready(function() {
             $('#sum-5').append(gen_p_tag(parseInt(lucky10_count - 1) + 1, name, customers[lucky10[lucky10_count - 1]]['No']));
         },counttime5 * 100 + 100);
     }
-    $("#export").click(function() {
-        let data1 = [["เลขที่นั่ง","รายชื่อ"]]
-        for(let i in lucky1) {
-            data1.push([customers[lucky1[i]]['No'], customers[lucky1[i]]['Firstname'] + ' ' + customers[lucky1[i]]['Lastname']]);
+    function random20() {
+        lucky20 = lucky20.concat(get_ramdom(5));
+        $('#sum-20').empty();
+        $('#sum-20').append('<h2>รายชื่อผู้โชคดี</h2>');
+        for(let i in lucky20) {
+            if (i < lucky20_count - 5) {
+                let name = get_name(lucky20[i]);
+                $('#sum-20').append(gen_p_tag(parseInt(i) + 1, name, customers[lucky20[i]]['No']));
+            }
         }
-        var workbook = XLSX.utils.book_new(), worksheet1 = XLSX.utils.aoa_to_sheet(data1);
-        workbook.SheetNames.push("ผู้โชคดี1");
-        workbook.Sheets["ผู้โชคดี1"] = worksheet1;
+        let counttime1 = 50;
+        let counttime2 = 60;
+        let counttime3 = 70;
+        let counttime4 = 80;
+        let counttime5 = 90;
+        var runningRender1 = setInterval(function(){
+            if (counttime1 > 0) {
+                run_number('#num_20_1');
+            }
+            counttime1--
+        }, 100);
+        var runningRender2 = setInterval(function(){
+            if (counttime2 > 0) {
+                run_number('#num_20_2');
+            }
+            counttime2--
+        }, 100);
+        var runningRender3 = setInterval(function(){
+            if (counttime3 > 0) {
+                run_number('#num_20_3');
+            }
+            counttime3--
+        }, 100);
+        var runningRender4 = setInterval(function(){
+            if (counttime4 > 0) {
+                run_number('#num_20_4');
+            }
+            counttime4--
+        }, 100);
+        var runningRender5 = setInterval(function(){
+            if (counttime5 > 0) {
+                run_number('#num_20_5');
+            }
+            counttime5--
+        }, 100);
+        setTimeout(function(){
+            clearInterval(runningRender1);
+            set_number('#num_20_1', customers[lucky20[lucky20_count - 5]]['No']);
+            let name = get_name(lucky20[lucky20_count - 5]);
+            $('#sum-20').append(gen_p_tag(parseInt(lucky20_count - 5) + 1, name, customers[lucky20[lucky20_count - 5]]['No']));
+        },counttime1 * 100 + 100);
+        setTimeout(function(){
+            clearInterval(runningRender2);
+            set_number('#num_20_2', customers[lucky20[lucky20_count - 4]]['No']);
+            let name = get_name(lucky20[lucky20_count - 4]);
+            $('#sum-20').append(gen_p_tag(parseInt(lucky20_count - 4) + 1, name, customers[lucky20[lucky20_count - 4]]['No']));
+        },counttime2 * 100 + 100);
+        setTimeout(function(){
+            clearInterval(runningRender3);
+            set_number('#num_20_3', customers[lucky20[lucky20_count - 3]]['No']);
+            let name = get_name(lucky20[lucky20_count - 3]);
+            $('#sum-20').append(gen_p_tag(parseInt(lucky20_count - 3) + 1, name, customers[lucky20[lucky20_count - 3]]['No']));
+        },counttime3 * 100 + 100);
+        setTimeout(function(){
+            clearInterval(runningRender4);
+            set_number('#num_20_4', customers[lucky20[lucky20_count - 2]]['No']);
+            let name = get_name(lucky20[lucky20_count - 2]);
+            $('#sum-20').append(gen_p_tag(parseInt(lucky20_count - 2) + 1, name, customers[lucky20[lucky20_count - 2]]['No']));
+        },counttime4 * 100 + 100);
+        setTimeout(function(){
+            clearInterval(runningRender5);
+            set_number('#num_20_5', customers[lucky20[lucky20_count - 1]]['No']);
+            let name = get_name(lucky20[lucky20_count - 1]);
+            $('#sum-20').append(gen_p_tag(parseInt(lucky20_count - 1) + 1, name, customers[lucky20[lucky20_count - 1]]['No']));
+        },counttime5 * 100 + 100);
+    }
+    $("#export").click(function() {
+        // let data1 = [["เลขที่นั่ง","รายชื่อ"]]
+        // for(let i in lucky1) {
+        //     data1.push([customers[lucky1[i]]['No'], customers[lucky1[i]]['Firstname'] + ' ' + customers[lucky1[i]]['Lastname']]);
+        // }
+        // var workbook = XLSX.utils.book_new(), worksheet1 = XLSX.utils.aoa_to_sheet(data1);
+        // workbook.SheetNames.push("ผู้โชคดี1");
+        // workbook.Sheets["ผู้โชคดี1"] = worksheet1;
+
         let data3 = [["เลขที่นั่ง","รายชื่อ"]]
         for(let i in lucky3) {
             data3.push([customers[lucky3[i]]['No'], customers[lucky3[i]]['Firstname'] + ' ' + customers[lucky3[i]]['Lastname']]);
         }
         var worksheet3 = XLSX.utils.aoa_to_sheet(data3);
-        workbook.SheetNames.push("ผู้โชคดี3");
-        workbook.Sheets["ผู้โชคดี3"] = worksheet3;
+        workbook.SheetNames.push("ผู้โชคดี 2 ท่าน");
+        workbook.Sheets["ผู้โชคดี 2 ท่าน"] = worksheet3;
+
         let data10 = [["เลขที่นั่ง","รายชื่อ"]]
         for(let i in lucky10) {
             data10.push([customers[lucky10[i]]['No'], customers[lucky10[i]]['Firstname'] + ' ' + customers[lucky10[i]]['Lastname']]);
         }
         var worksheet10 = XLSX.utils.aoa_to_sheet(data10);
-        workbook.SheetNames.push("ผู้โชคดี10");
-        workbook.Sheets["ผู้โชคดี10"] = worksheet10;
+        workbook.SheetNames.push("ผู้โชคดี 10 ท่าน");
+        workbook.Sheets["ผู้โชคดี 10 ท่าน"] = worksheet10;
+
+        let data20 = [["เลขที่นั่ง","รายชื่อ"]]
+        for(let i in lucky20) {
+            data10.push([customers[lucky20[i]]['No'], customers[lucky20[i]]['Firstname'] + ' ' + customers[lucky20[i]]['Lastname']]);
+        }
+        var worksheet20 = XLSX.utils.aoa_to_sheet(data10);
+        workbook.SheetNames.push("ผู้โชคดี 20 ท่าน");
+        workbook.Sheets["ผู้โชคดี 20 ท่าน"] = worksheet20;
 
         XLSX.writeFile(workbook, "ผู้โชคดี.xlsx");
         var xlsblob = new Blob(
@@ -286,13 +375,34 @@ $(document).ready(function() {
             count += 1;
         }, 500);
     }
+    function changeRandomBtn20(name, callback) {
+        let count = 1;
+        ramdomInterval = setInterval(() => {
+            if (count > 0 && count < 7){
+                if (count % 2 == 0)
+                    $(name).css('background-image',"url('./random_btn-" + (4 - parseInt(count/2)) + ".png')");
+                else 
+                    $(name).css('background-image',"url('./empty_btn.png')");
+            }
+            else if (count == 7) {
+                $(name).css('background-image',"url('./random_btn.png')");
+                lucky20_count += 5;
+                if (lucky20_count == lucky20_limit)
+                    $(name).attr('disabled', 'disabled');
+                clearInterval(ramdomInterval);
+                callback();
+            }
+            count += 1;
+        }, 500);
+    }
     $("#start-btn").click(function() {
         if(/*((window.fullScreen) || (window.innerWidth == screen.width && window.innerHeight == screen.height)) && */$('#customer-file')[0].files.length > 0) {
             readFile();
             $('#container-prepare').hide();
-            $('#container-1').show();
-            $('#container-3').hide();
+            $('#container-1').hide();
+            $('#container-3').show();
             $('#container-5').hide();
+            $('#container-20').hide();
             $('#container-summary').hide();
         } else {
             alert('Please upload file.');
@@ -306,6 +416,9 @@ $(document).ready(function() {
     });
     $("#random-5-btn").click(function() {
         changeRandomBtn5('#random-5-btn', random5);
+    });
+    $("#random-20-btn").click(function() {
+        changeRandomBtn20('#random-20-btn', random20);
     });
     $("#next-to-3").click(function() {
         $('#container-1').hide();
@@ -323,24 +436,34 @@ $(document).ready(function() {
         $('#container-1').hide();
         $('#container-3').hide();
         $('#container-5').show();
+        $('#container-20').hide();
         $('#container-summary').hide();
     });
     $("#prev-to-3").click(function() {
         $('#container-1').hide();
         $('#container-3').show();
         $('#container-5').hide();
+        $('#container-20').hide();
+        $('#container-summary').hide();
+    });
+    $("#next-to-20").click(function() {
+        $('#container-1').hide();
+        $('#container-3').hide();
+        $('#container-5').hide();
+        $('#container-20').show();
         $('#container-summary').hide();
     });
     $("#summary-btn").click(function() {
         $('#container-1').hide();
         $('#container-3').hide();
         $('#container-5').hide();
+        $('#container-20').hide();
         $('#container-summary').show();
-        $('.summary-1').empty();
-        for(let i in lucky1) {
-            let name = get_name(lucky1[i]);
-            $('.summary-1').append(gen_p_tag(parseInt(i) + 1, name, customers[lucky1[i]]['No']));
-        }
+        // $('.summary-1').empty();
+        // for(let i in lucky1) {
+        //     let name = get_name(lucky1[i]);
+        //     $('.summary-1').append(gen_p_tag(parseInt(i) + 1, name, customers[lucky1[i]]['No']));
+        // }
         $('.summary-3').empty();
         for(let i in lucky3) {
             let name = get_name(lucky3[i]);
@@ -354,11 +477,27 @@ $(document).ready(function() {
             if (parseInt(i) >= 5) id = '#summary-10-right';
             $(id).append(gen_p_tag(parseInt(i) + 1, name, customers[lucky10[i]]['No']));
         }
+        $('#summary-20-left').empty();
+        $('#summary-20-right').empty();
+        for(let i in lucky20) {
+            let name = get_name(lucky20[i]);
+            let id = '#summary-20-left';
+            if (parseInt(i) >= 10) id = '#summary-20-right';
+            $(id).append(gen_p_tag(parseInt(i) + 1, name, customers[lucky20[i]]['No']));
+        }
     });
     $("#prev-to-5").click(function() {
         $('#container-1').hide();
         $('#container-3').hide();
         $('#container-5').show();
+        $('#container-20').hide();
+        $('#container-summary').hide();
+    });
+    $("#prev-to-20").click(function() {
+        $('#container-1').hide();
+        $('#container-3').hide();
+        $('#container-5').hide();
+        $('#container-20').show();
         $('#container-summary').hide();
     });
 });
